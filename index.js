658471c122824285969cadfe673f77009cbc4a06
Konstantin Chuykov <chuikoff.ru@gmail.com>
2020-01-12 07:52:17 +1200

    Added new routes and checking table exists

--- index.js
+++ index.js
@@ -18,8 +18,44 @@ app.get('/', async (req, res) => {
   
 })
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+app.get('/:id', async (req, res) => {
 
-//Run app, then load http://localhost:3000 in a browser to see the output.
+  //const taskId = req.params.id
+
+})
+
+app.post('/add', async (req, res) => {
+
+  //const fields = req.body
+
+})
+
+app.delete('/:id', async (req, res) => {
+
+  //const taskId = req.params.id
+
+})
+
+app.listen(3000, async () => {
+
+  try {
+
+    //Если таблицы задач не существует в базе данных - создаем новую
+
+    const tasksTableExist = await query(`SHOW TABLES FROM grqAt1o6uw LIKE "tasks"`)
+
+    if(tasksTableExist.length === 0) {
+      await query(`CREATE TABLE IF NOT EXISTS tasks (
+        id INT NOT NULL AUTO_INCREMENT , 
+        text VARCHAR(255) NOT NULL , 
+        priority TINYINT(100) NOT NULL , 
+        PRIMARY KEY (id)) ENGINE = InnoDB;`)
+    }
+
+  } catch (error) {
+    
+  }
+
+  console.log('Example app listening on port 3000!')
+  
+})
